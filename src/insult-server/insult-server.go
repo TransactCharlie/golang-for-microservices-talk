ef5fd1c3b3432c1af31110a1285d2c60e377adc8
Charlie Gildawie <charlieg@monzo.com>
2018-03-21 20:06:44 +0000

    insult server

---
+++ src/insult-server/insult-server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"time"
+	"net/http"
+	"log"
+)
+
+var principals = []string {
+	"bampot",
+	"bawbag",
+	"clipe",
+	"dobber",
+	"jakey",
+}
+
+var adjectives = []string{
+	"crazy",
+	"steamin",
+	"connivin",
+	"ugly",
+	"howlin",
+}
+
+var actions = []string {
+	"Awa' n boil yer head",
+	"Shut yer gob",
+	"Yer aff yer heid",
+}
+
+func choose_from(s []string) string {
+	return s[rand.Intn(len(s))]
+}
+
+func gen_insult() string {
+	time.Sleep(time.Second)
+	return fmt.Sprintf(
+		"%s ya %s %s\n",
+		choose_from(actions),
+		choose_from(adjectives),
+		choose_from(principals),
+		)
+}
+
+func insultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, gen_insult())
+}
+
+func main() {
+	// Init the random seed
+	rand.Seed(time.Now().Unix())
+	http.HandleFunc("/insult", insultHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
