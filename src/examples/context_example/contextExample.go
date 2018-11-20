32d993d6cc2bdebac074cf373dda9c4ec4141dac
Charlie Gildawie <charlieg@monzo.com>
2018-11-20 16:02:03 +0000

    Context Example

---
+++ src/examples/context_example/contextExample.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func worker(ctx context.Context, msg string) {
+	select {
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+	default:
+		fmt.Println(msg)
+	}
+}
+
+func main() {
+	d := time.Now().Add(time.Second * 2)
+	bg := context.Background()
+	ctx, _ := context.WithDeadline(bg, d)
+
+	worker(ctx, "Hello")
+	time.Sleep(time.Second * 5)
+	worker(ctx, "Hello Again!")
+}
